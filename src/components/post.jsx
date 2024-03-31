e7f137dd5c56caba055cd006ec9a0c3397771afc
HolidayGuy21 <14449@coderacademy.edu.au>
2024-03-31 14:48:49 +1100

    added get specific page route

--- src/components/post.jsx
+++ src/components/post.jsx
@@ -2,22 +2,43 @@ import { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 
 const Post = ({ JWT, setJWT }) => {
+const [response, setResponse] = useState('')
+const [loading, setLoading] = useState(false)
+
 const { postid } = useParams();
 let idNumber = parseInt(postid);
 console.log(idNumber)
 
-fetch(`http://localhost:5000/posts/${postid}`, {mode: 'cors'})
-    .then(function(response) {
-      return response.json();
-    })
-    .then(function(response) {
-      console.log(response);
-    });
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:5000/posts/${postid}`);
+      const result = await response.json();
+      setResponse(result);
+      setLoading(true);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  fetchData(); 
+}, []); 
 
     return (
-        <>
-            <h2>Post Component</h2>
-        </>
+    <>
+      <div>
+        {loading ? (
+          <>
+            <h1>{response.title}</h1>
+            <p>{response.content}</p>
+            <p>Written by {response.user.username}</p>
+            <p>Published on {response.date_published}</p>
+          </>
+        ) : (
+          <h2>Loading Blog...</h2>
+        )}
+      </div>
+    </>
     )
 
 }
